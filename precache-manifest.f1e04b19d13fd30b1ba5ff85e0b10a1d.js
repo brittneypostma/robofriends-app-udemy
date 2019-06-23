7a40ecc5aa162eb38ba8248011cd7ecacbe9dcac
Brittney Postma <brittney.postma@hotmail.com>
2019-06-23 13:56:46 -0400

    Updates

---
+++ precache-manifest.f1e04b19d13fd30b1ba5ff85e0b10a1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59325cbdc17ab291dc7e73319fbe7efa",
+    "url": "/robofriends-app-udemy/index.html"
+  },
+  {
+    "revision": "6be1da2afe149c2f4471",
+    "url": "/robofriends-app-udemy/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4fabbe3215c9c42f0fe3",
+    "url": "/robofriends-app-udemy/static/css/main.ba6eec1f.chunk.css"
+  },
+  {
+    "revision": "6be1da2afe149c2f4471",
+    "url": "/robofriends-app-udemy/static/js/2.aa821b8d.chunk.js"
+  },
+  {
+    "revision": "4fabbe3215c9c42f0fe3",
+    "url": "/robofriends-app-udemy/static/js/main.aeffa0a6.chunk.js"
+  },
+  {
+    "revision": "5f7e30a3e1b1eacea4cc",
+    "url": "/robofriends-app-udemy/static/js/runtime~main.52d37670.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-app-udemy/static/media/SEGA.12a3db8b.woff"
+  }
+]);
